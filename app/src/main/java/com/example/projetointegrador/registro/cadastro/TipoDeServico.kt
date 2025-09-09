be9af8a36c157a9b5cc6ca4c1020bf43754523df
joaov <joaovitoralcure@gmail.com>
2025-09-09 15:56:23 -0300

    Tela de splash, inicial, cadastro, login e recuperação de senha.

--- app/src/main/java/com/example/projetointegrador/registro/cadastro/TipoDeServico.kt
+++ app/src/main/java/com/example/projetointegrador/registro/cadastro/TipoDeServico.kt
@@ -1,9 +1,7 @@
 package com.example.projetointegrador.registro.cadastro
 
 import android.os.Bundle
-import android.text.Editable
 import android.text.InputFilter
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
